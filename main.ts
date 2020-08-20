728615891b3fc021d2d1f3aa8925267f63e5e708
rhubarbdog <33667064+rhubarbdog@users.noreply.github.com>
2020-08-20 04:33:23 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+let temp = 0
+let n = 5
+let factorial = 1
+for (let index = 0; index <= n - 1; index++) {
+    temp = index + 1
+    factorial = factorial * temp
+}
+basic.showNumber(factorial)
+basic.forever(function () {
+	
+})
